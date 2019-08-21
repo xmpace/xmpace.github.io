36eca287380b70cb6bd9838524609a487b122d66
xiaomi <xmpace@gmail.com>
2019-08-21 20:11:18 +0800

    add post <<登录 token 的设计>>

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,30 @@
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="tooltip"]').tooltip();
+
+  $(".ym-post__content--custom img").each(function(idx) {
+    var thisImg = $(this);
+    // 图片标题
+    thisImg.after(function() {
+      return "<div align=\"center\" class=\"img-text\">" + this.alt + "</div>";
+    });
+
+    // 默认居中
+    if (!thisImg.attr("align")) {
+      thisImg.wrap("<div align=\"center\"></div>");
+    }
+
+    // mac retina的图片默认缩小1倍
+    var os = thisImg.attr("os") || "win";
+    var specifiedWidth = thisImg.attr("width");
+    var specifiedHeight = thisImg.attr("height");
+
+    if (!specifiedHeight && !specifiedWidth) {
+      var imgWidth = thisImg.prop("width");
+      var maxWidth = $(".ym-post__content--custom").innerWidth();
+      var bestWidth = imgWidth / 2 > maxWidth ? maxWidth : imgWidth / 2;
+      thisImg.prop("width", bestWidth);
+    }
+  });
+
+  
 })

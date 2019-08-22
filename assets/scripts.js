e2e42269c89eafdafb32508563c29f0d91057dae
xiaomi <xmpace@gmail.com>
2019-08-22 11:00:09 +0800

    fix image load event handler

--- assets/scripts.js
+++ assets/scripts.js
@@ -15,18 +15,23 @@ $(function () {
 
     // mac retina的图片默认缩小1倍
     var os = thisImg.attr("os") || "win";
-    var specifiedWidth = thisImg.attr("width");
-    var specifiedHeight = thisImg.attr("height");
-
-    if (!specifiedHeight && !specifiedWidth) {
-      var imgWidth = thisImg.prop("naturalWidth");
-      var maxWidth = $(".ym-post__content--custom").innerWidth();
-      if (os === 'mac') {
-        imgWidth = imgWidth / 2;
+    thisImg[0].onload = function() {
+      var specifiedWidth = thisImg.attr("width");
+      var specifiedHeight = thisImg.attr("height");
+  
+      if (!specifiedHeight && !specifiedWidth) {
+        var imgWidth = thisImg.prop("naturalWidth");
+        var maxWidth = $(".ym-post__content--custom").innerWidth();
+        if (os === 'mac') {
+          imgWidth = imgWidth / 2;
+        }
+        var bestWidth = imgWidth > maxWidth ? maxWidth : imgWidth;
+        thisImg.prop("width", bestWidth);
       }
-      var bestWidth = imgWidth > maxWidth ? maxWidth : imgWidth;
-      thisImg.prop("width", bestWidth);
     }
+
+    // trigger img onload event
+    thisImg.prop("src", thisImg.prop("src"));
   });
 
   
